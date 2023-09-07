25a4bba820413cf5699f3149c487445c1801dc80
Israel <imbernal9203@gmail.com>
2023-09-07 19:27:56 -0400

    Cognito Middleware

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,11 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@double2.5i48ses.mongodb.net/master-api`;
 
+/** AWS CONFIG */
+const AWS_REGION = process.env.AWS_REGION;
+const AWS_COGNITO_POOL_ID = process.env.AWS_COGNITO_POOL_ID;
+const AWS_COGNITO_APP_CLIENT_ID = process.env.AWS_COGNITO_APP_CLIENT_ID;
+
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
 
 export const config = {
@@ -14,5 +19,10 @@ export const config = {
     },
     server: {
         port: SERVER_PORT
+    },
+    aws: {
+        region: AWS_REGION,
+        clientID: AWS_COGNITO_APP_CLIENT_ID,
+        poolId: AWS_COGNITO_POOL_ID
     }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
+// @ts-ignore
+import CognitoExpress from 'cognito-express';
 import { config } from './config/config';
 import Logger from './library/Logging';
 import authorRoutes from './routes/Author';
+import AWS from 'aws-sdk';
+AWS.config.region = config.aws.region; // e.g., us-east-1
 
 const router = express();
 
@@ -51,8 +55,36 @@ const StartServer = () => {
         next();
     });
 
+    //Initializing CognitoExpress constructor
+    const cognitoExpress = new CognitoExpress({
+        region: 'us-east-1',
+        cognitoUserPoolId: config.aws.poolId,
+        tokenUse: 'access', //Possible Values: access | id
+        tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
+    });
+
+    //Our middleware that authenticates all APIs under our 'authenticatedRoute' Router
+    router.use(function (req, res, next) {
+        //I'm passing in the access token in header under key accessToken
+        let accessTokenFromClient = req.headers.accesstoken;
+
+        //Fail if token not present in header.
+        if (!accessTokenFromClient) return res.status(401).send('Access Token missing from header');
+
+        cognitoExpress.validate(accessTokenFromClient, function (err: any, response: any) {
+            //If API is not authenticated, Return 401 with error message.
+            if (err) return res.status(401).send(err);
+
+            //Else API has been authenticated. Proceed.
+            res.locals.user = response;
+
+            next();
+        });
+    });
+
     /** Routes */
     router.use('/authors', authorRoutes);
+
     /** Error handling */
 
     router.use((req, res, next) => {
